3e5b6e6378014341bce506e5f7578579d04972b7
Nádia Oliveira <nasilvah21@gmail.com>
2019-10-23 20:36:19 -0300

    Todas as funções implementadas

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 new Vue({
     el: '#app',
     data: {
+        instructionsControll: false,
+        counter: 0,
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -15,9 +18,44 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+            this.counter = 0
+            this.logs = []
+        },
+        instructions() {
+            this.instructionsControll = true
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
+        },
+        hurt(prop, min, max, especial, source, target, cls) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         }
     },
     watch: {
-
+        hasResult(value) {
+            if(value) this.running = false
+        }
     }
 })
